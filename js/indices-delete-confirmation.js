a085c45376ccb015e21ab707776883d1a6252b44
MChen <ming.chen0919@gmail.com>
2017-02-16 17:24:39 -0500

    update jquery stuff

--- js/indices-delete-confirmation.js
+++ js/indices-delete-confirmation.js
@@ -2,14 +2,13 @@
  * Created by mingchen on 1/31/17.
  */
 (function ($) {
-    Drupal.behaviors = Drupal.behaviors || {}
-
+    Drupal.behaviors = Drupal.behaviors || {};
 
     Drupal.behaviors.indices_delete_confirmation = {
         attach: function (context, settings) {
             $(document).on('click', '#indices-delete-confirmation', function(e) {
 
-                if (confirm("Are you sure? This step can not be undo.")) {
+                if (confirm("Warning: you can not undo this step!")) {
                 }
                 else {
                     e.preventDefault();
